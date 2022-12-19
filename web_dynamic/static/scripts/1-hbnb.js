fab43b2bfb47ebf900726122802bf3b7f1fa9c60
RubberizedDuck <dyl_2536@hotmail.com>
2022-12-19 15:56:59 +1100

    Change from push to append

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -9,7 +9,7 @@ $(window).on('load', function () {
     }
     const isChecked = [];
     for (const check in checkList) {
-      isChecked.push(checkList[check]);
+      isChecked.append(checkList[check]);
     }
     $('.amenities h4').text(isChecked.join(', '));
   });
