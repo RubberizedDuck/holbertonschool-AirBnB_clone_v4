28719d65828db2e9d6bec56711f2f9b6e0b06d33
RubberizedDuck <dyl_2536@hotmail.com>
2022-12-19 15:50:52 +1100

    Change data to attr

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -3,9 +3,9 @@ $(window).on('load', function () {
   $("input[type='checkbox']").change(function () {
     const checkList = {};
     if ($(this).is(':checked')) {
-      checkList[$(this).data('id')] = $(this).data('name');
+      checkList[$(this).attr('data-id')] = $(this).attr('data-name');
     } else {
-      delete checkList[$(this).data('id')];
+      delete checkList[$(this).attr('data-id')];
     }
     const checked = [];
     for (const check in checkList) {
