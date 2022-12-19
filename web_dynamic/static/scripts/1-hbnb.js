0cbb6ee1354cba93b318695c33f8671db598f623
RubberizedDuck <dyl_2536@hotmail.com>
2022-12-19 15:55:39 +1100

    Fix: change from change to click function

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,16 +1,16 @@
 $(window).on('load', function () {
   $('input:checkbox').css('margin-right', '10px');
-  $("input[type='checkbox']").change(function () {
+  $("input[type='checkbox']").click(function () {
     const checkList = {};
     if ($(this).is(':checked')) {
       checkList[$(this).attr('data-id')] = $(this).attr('data-name');
     } else if (!$(this).is(':checked')) {
       delete checkList[$(this).attr('data-id')];
     }
-    const checked = [];
+    const isChecked = [];
     for (const check in checkList) {
-      checked.push(checkList[check]);
+      isChecked.push(checkList[check]);
     }
-    $('.amenities h4').text(checked.join(', '));
+    $('.amenities h4').text(isChecked.join(', '));
   });
 });
