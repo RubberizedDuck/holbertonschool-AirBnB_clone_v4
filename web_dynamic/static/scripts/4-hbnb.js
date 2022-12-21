a28f88994add709aa90d97ce1022e71d1c84931e
Gotmished <5002@holbertonstudents.com>
2022-12-21 19:17:28 +1100

    Re-factorising

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -40,9 +40,9 @@ function searchPlaces () {
     $.ajax({
       type: 'POST',
       url: 'http://cc5333933a49.6ed948a4.hbtn-cod.io:5001/api/v1/places_search/',
-      data: JSON.stringify({
-        amenities: Object.values(finalUse)
-      }),
+      data: JSON.stringify(
+        Object.values(finalUse)
+      ),
       dataType: 'json',
       contentType: 'application/json',
       success: function (data) {
