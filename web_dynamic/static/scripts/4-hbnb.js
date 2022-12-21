5da4c2edddecda5b096ef63bab805df093da76ae
Gotmished <5002@holbertonstudents.com>
2022-12-21 18:01:33 +1100

    Re-factorising

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -3,7 +3,7 @@ $(document).ready(init);
 function init () {
   const useThis = checkedList();
   apiStatus();
-  searchPlaces(amenityChecks);
+  searchPlaces(useThis);
 }
 
 function checkedList () {
